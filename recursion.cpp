4e7d59aab22bd73cfae60bda8b04791cfe562870
Shaunak-Shandilya <shaunak.shandilya@outlook.com>
2025-07-12 16:07:02 +0530

    Create recursion.cpp

---
+++ recursion.cpp
@@ -0,0 +1,20 @@
+void fun1(int n){
+  if (n>0){
+    printf("%d",n);
+    fun1(n-1);
+  }
+}
+
+void fun2(int n){
+  if (n>0){
+    fun2(n-1);
+    printf("%d",n);
+  }
+}
+
+void main(){
+  int x = 3;
+  fun1(3); //321
+  fun2(3); //123
+  
+}
