fa5cb6b4bd4e660480251dbda6a583e1c64deb8e
Shaunak-Shandilya <shaunak.shandilya@outlook.com>
2025-07-13 00:09:22 +0530

    Update math_by_recursion.cpp

--- math_by_recursion.cpp
+++ math_by_recursion.cpp
@@ -52,4 +52,18 @@ int fib(int n){
 return fib(n-2)+fib(n-1);
 }
 
+//memorizing fibonacci series
 
+int F[10];
+
+int fib(int n){
+    if(n<=1){
+        F[n] = n;
+        return n;
+    }
+    else{
+        if (F[n-2] == -1) F[n-2] = fib(n-2);
+        if (F[n-1] == -1) F[n-1] = fib(n-1);
+        return F[n-2]+F[n-1];
+    }
+}
